44904187b2d1cdc9211fb4d749be62b0a8031122
satyamahendra <satyamahendra09@gmail.com>
2026-04-19 14:33:29 +0700

    feat: prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image"
 import {Button} from "@/components/ui/button"
+import prisma from "@/lib/prisma/client"
 
-export default function Home() {
+export default async function Home() {
+    const data = await prisma.test.findMany()
+
     return (
         <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
             <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
